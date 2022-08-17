b6da46265e54f67b2ca8904485796ed0bb0beb23
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-17 23:02:34 +0900

    feat: Row 컴포넌트 추가

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Row from "./components/Row";
+import { requests } from "./api";
+
+function App() {
+  return (
+    <div className="app">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}></Row>
+    </div>
+  );
+}
+
+export default App;
