bf68cde87b4c88718824a77a2f7c59568b00f19e
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-18 14:27:20 +0900

    feat : youtube 예고편 미리보기 기능 추가

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,13 +2,13 @@ const API_KEY = "a044f209e617547a8b4d264a7a913f23";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=ko`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=ko`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=ko`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=ko`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=ko`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=ko`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749&language=ko`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99&language=ko`,
 };
 
 export default requests;
--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { axios } from "../../api";
 import "./style.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -17,6 +20,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?v=trailerUrl
+          const urlParams = new URLSearchParams(new URL(url).search);
+
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -25,11 +51,13 @@ function Row({ title, fetchUrl, isLargeRow }) {
           <img
             className={`row__poster ${isLargeRow ? "row__posterLarge" : ""}`}
             key={movie.id}
+            onClick={() => handleClick(movie)}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
