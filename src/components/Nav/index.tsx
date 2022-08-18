38d88d27bbc34f7d18666b3442b6dd133eed6f0a
tjdgus3160 <tjdgus3160@gmail.com>
2022-08-18 18:14:54 +0900

    fix : 정적 이미지 사용

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -16,16 +16,8 @@ function Nav() {
 
   return (
     <nav className={`nav ${show ? "nav__black" : ""}`}>
-      <img
-        className="nav__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png?20190206123158"
-        alt="Netflix Logo"
-      />
-      <img
-        className="nav__avatar"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
-        alt="Netflix Logo"
-      />
+      <img className="nav__logo" src="/images/logo.png" alt="Netflix Logo" />
+      <img className="nav__avatar" src="/images/avatar.png" alt="Netflix Logo" />
     </nav>
   );
 }
